e7ba7242cb52f0bf52ade5f90cf6cf919f957ee1
Amirhossein Maleki <134789169+amirmalakie@users.noreply.github.com>
2023-11-20 20:24:10 +0300

    Update parsedashboard.js

--- parsedashboard.js
+++ parsedashboard.js
@@ -1,36 +1,77 @@
 require('dotenv').config({ path: './config.env' });
-var express = require('express');
-var ParseDashboard = require('parse-dashboard');
+const express = require('express');
+const ParseDashboard = require('parse-dashboard');
+const ParseServer = require('parse-server').ParseServer;
+const fs = require('fs');
+const https = require('https');
 
-var ip = process.env.IP;
-var appname = process.env.APP_NAME;
-var APP_ID = process.env.APP_ID;
-var MASTERKEY = process.env.MASTERKEY;
-var CLIENT_KEY = process.env.CILENT_KEY;
+const ip = process.env.IP;
+const appname = process.env.APP_NAME;
+const APP_ID = process.env.APP_ID;
+const MASTERKEY = process.env.MASTERKEY;
+const CLIENT_KEY = process.env.CLIENT_KEY;
 
-var options = { allowInsecureHTTP: true };
+// Parse Server Configuration
+const parseServerOptions = {
+  databaseURI: `mongodb://localhost:27017/${appname}`,
+  cloud: './cloud/main.js',
+  appId: APP_ID,
+  masterKey: MASTERKEY,
+  clientKey: CLIENT_KEY,
+  serverURL: `https://${ip}:1337/parse`,
+  liveQuery: {
+    classNames: ["Test", "Comments"]
+  },
+  masterKeyIps: ["0.0.0.0/0"],
+  collections: ['_Installation'],
+  production: false,
+  logsFolder: '/root/parse/',
+  push: {
+    android: {
+      senderId: 'YOUR_SENDER_ID',
+      apiKey: 'YOUR_API_KEY',
+      appPackageName: 'com.your.package.name'
+    }
+  }
+};
+
+const parseServer = new ParseServer(parseServerOptions);
+
+// Parse Dashboard Configuration
+const dashboardOptions = { allowInsecureHTTP: true };
 
 const dashboard = new ParseDashboard({
-    "apps": [
-      {
-        "serverURL": `http://${ip}:1337/parse`,
-        "appId": APP_ID,
-        "masterKey": MASTERKEY,
-        "appName": appname,
-        "clientKey": CLIENT_KEY,
+  "apps": [
+    {
+      "serverURL": `https://${ip}:1337/parse`,
+      "appId": APP_ID,
+      "masterKey": MASTERKEY,
+      "appName": appname,
+      "clientKey": CLIENT_KEY,
     }
-    ],
-    "users": [
-      {
-        "user": "amirmalakie",
-        "pass": "iranianseda"
-      }
-    ]
-  }, options);
+  ],
+  "users": [
+    {
+      "user": "amirmalakie",
+      "pass": "iranianseda"
+    }
+  ]
+}, dashboardOptions);
 
-var app = express();
+const app = express();
 
 app.use('/dashboard', dashboard);
 
-var httpServer = require('http').createServer(app);
-httpServer.listen(4040);
+// Set up HTTPS server
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/app.tasktome.ir/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/app.tasktome.ir/fullchain.pem'),
+};
+
+const httpsServer = https.createServer(httpsOptions, app);
+
+httpsServer.listen(443, function () {
+  console.log('HTTPS server listening on port 443.');
+});
+
+parseServer.start();
